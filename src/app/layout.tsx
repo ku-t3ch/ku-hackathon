cdf815efe1501837930763b09321a7b390fd010b
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-10-11 16:30:07 +0700

    update!: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
+import './globals.css';
+import type { Metadata } from 'next';
 
-import "./globals.css";
-import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import { Providers } from './providers';
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
-import { Inter } from "next/font/google";
-import { Providers } from "./providers";
-import React from "react";
-import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: "KU Hackathon",
-    description: "KU Hackathon กระเทาะแอปนิสิต โดยนิสิต เพื่อนิสิต",
+  title: 'KU Hackathon',
+  description: 'KU Hackathon กระเทาะแอปนิสิต โดยนิสิต เพื่อนิสิต',
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    
-    return (
-        <html lang="en" className="dark">
-            <body className={inter.className}>
-                <Providers>
-                    <Navbar />
-                    <div className="flex flex-col pb-10">{children}</div>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <div className="flex flex-col pb-10">{children}</div>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
 }
