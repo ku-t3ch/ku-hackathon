e273198c18c4748c188ba8f77752e9acb6a5a76b
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-01 21:29:29 +0700

    update!: redesign partners & clean code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    reactStrictMode: false,
+  output: 'standalone',
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.tech.nisit.ku.ac.th',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
