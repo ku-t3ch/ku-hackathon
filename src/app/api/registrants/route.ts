4adf000c615afafa102dd988e279a08af576a1ee
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-11-04 14:01:19 +0700

    doc: add example response to registrants route

--- src/app/api/registrants/route.ts
+++ src/app/api/registrants/route.ts
@@ -4,5 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: Request | NextRequest) {
     let res = await getRegistrants();
 
+    /**
+     * Response is in JSON, example:
+     * {
+     *      designers: 16,
+     *      developers: 32
+     * }
+     */
     return NextResponse.json(res, {status: 200});
 }
