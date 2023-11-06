1c3e6c9fc015c3170fdb2877c40a4f0cca719065
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-06 23:06:20 +0700

    fear(api): make local cache

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,3 +1,4 @@
+import { getCache, setCache } from '@/utils/Cache';
 import { getListedIssues } from '@/utils/GSheetToIssues';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -5,8 +6,17 @@ export const dynamic = 'force-dynamic';
 
 // To handle a GET request to /api
 export async function GET(request: Request | NextRequest) {
-  // Do whatever you want
-  let result = await getListedIssues();
+  const cached = getCache('issues');
 
-  return NextResponse.json(result, { status: 200 });
+  if (cached) {
+    return NextResponse.json(cached, { status: 200 });
+  }
+
+  // Do whatever you want
+  let res = await getListedIssues();
+
+  // set cache
+  setCache(60 * 10, 'issues', res);
+
+  return NextResponse.json(res, { status: 200 });
 }
