f22c5f937da105423a531b9e4dcc2208416a1c5d
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-06 19:59:26 +0700

    fix(api): response not update (cache)

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,11 +1,12 @@
-import { getListedIssues } from "@/utils/GSheetToIssues";
-import { NextApiRequest } from "next";
-import { NextRequest, NextResponse } from "next/server";
+import { getListedIssues } from '@/utils/GSheetToIssues';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const revalidate = 300;
 
 // To handle a GET request to /api
 export async function GET(request: Request | NextRequest) {
-    // Do whatever you want
-    let result = await getListedIssues();
+  // Do whatever you want
+  let result = await getListedIssues();
 
-    return NextResponse.json(result, { status: 200 });
+  return NextResponse.json(result, { status: 200 });
 }
