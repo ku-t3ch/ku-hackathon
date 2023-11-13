e18adc753f3c7f11ef73cb2a58aa1de252c663d3
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-13 18:54:32 +0700

    🐛 fix(navbar): fixnavbar not back to main page

--- src/components/Navbar/NavScrollLink.tsx
+++ src/components/Navbar/NavScrollLink.tsx
@@ -1,19 +1,17 @@
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { FC, ReactNode } from 'react';
 import { Link } from 'react-scroll';
-
 interface Props {
   to: string;
   className?: string;
-  onClick?: () => void;
   children?: ReactNode;
 }
 
-export const NavScrollLink: FC<Props> = ({
-  to,
-  className,
-  onClick,
-  children,
-}) => {
+export const NavScrollLink: FC<Props> = ({ to, className, children }) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <Link
       to={to}
@@ -22,7 +20,13 @@ export const NavScrollLink: FC<Props> = ({
       spy={true}
       className={`flex items-center font-medium hover:text-primary cursor-default ${className}`}
       activeClass="text-primary shadow-[inset_0_-2.5px]"
-      onClick={onClick}
+      onClick={() => {
+        if (pathname == '/') {
+          return;
+        }
+
+        router.push('/');
+      }}
     >
       {children}
     </Link>
