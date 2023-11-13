e39207fcdb38828b42e0ca06080ad89f8b505f55
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-13 19:50:27 +0700

    ♻️ refactor(home): change text to logo

--- src/components/Sections/HomeSection.tsx
+++ src/components/Sections/HomeSection.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 
 import BannerSlider from '../BannerSlider';
 import { FC, useState } from 'react';
+import Image from 'next/image';
 
 interface Props {}
 
@@ -58,65 +59,90 @@ const HackathonLogo: FC<{}> = () => {
   const highlight = [7, 8, 9, 10, 11, 12, 13, 14]; // index of slogan text
 
   return (
-    <div className="flex gap-2 md:gap-4 items-start">
-      <motion.div
-        initial={{
-          x: '100%',
-        }}
-        animate={{
-          x: 0,
-        }}
-        transition={{
-          delay: 0.1,
-          duration: 0.5,
-        }}
-        className="text-[17.5vw] md:text-[8rem] font-extrabold leading-none text-primary hackathon-glow"
-      >
-        KU
-      </motion.div>
+    <div className="flex flex-col items-center">
       <motion.div
         initial={{
           opacity: 0,
-          y: '5vw',
+          scale: 0.85,
         }}
         animate={{
           opacity: 1,
-          y: 0,
+          scale: 1,
         }}
         transition={{
-          delay: 0.65,
           duration: 0.5,
         }}
-        className="flex flex-col py-[1vw] md:py-[.5rem]"
       >
-        <div className="text-[8vw] md:text-[4rem] font-bold leading-none tracking-wider text-primary hackathon-glow">
-          Hackathon
-        </div>
-        <div className="text-[3.25vw] md:text-[1.65rem] tracking-winder">
-          <motion.span
-            initial="hidden"
-            animate="visible"
-            transition={{ staggerChildren: 0.1 }}
-            aria-hidden
-          >
-            {slogan.split('').map((char, idx) => {
-              return (
-                <motion.span
-                  key={idx}
-                  variants={sloganAnimation}
-                  className={
-                    highlight.includes(idx)
-                      ? 'text-[#e23776] font-semibold'
-                      : ''
-                  }
-                >
-                  {char}
-                </motion.span>
-              );
-            })}
-          </motion.span>
-        </div>
+        <Image
+          src={`${process.env.cdn}/main-logo.webp`}
+          width={0}
+          height={0}
+          alt="main-logo"
+          className="w-[300px] text-center"
+          unoptimized
+          priority
+        />
       </motion.div>
+      {/* <div className="flex gap-2 md:gap-4 items-start">
+        <motion.div
+          initial={{
+            x: '100%',
+          }}
+          animate={{
+            x: 0,
+          }}
+          transition={{
+            delay: 0.1,
+            duration: 0.5,
+          }}
+          className="text-[17.5vw] md:text-[8rem] font-extrabold leading-none text-primary hackathon-glow"
+        >
+          KU
+        </motion.div>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: '5vw',
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            delay: 0.65,
+            duration: 0.5,
+          }}
+          className="flex flex-col py-[1vw] md:py-[.5rem]"
+        >
+          <div className="text-[8vw] md:text-[4rem] font-bold leading-none tracking-wider text-primary hackathon-glow">
+            Hackathon
+          </div>
+          <div className="text-[3.25vw] md:text-[1.65rem] tracking-winder">
+            <motion.span
+              initial="hidden"
+              animate="visible"
+              transition={{ staggerChildren: 0.1 }}
+              aria-hidden
+            >
+              {slogan.split('').map((char, idx) => {
+                return (
+                  <motion.span
+                    key={idx}
+                    variants={sloganAnimation}
+                    className={
+                      highlight.includes(idx)
+                        ? 'text-[#e23776] font-semibold'
+                        : ''
+                    }
+                  >
+                    {char}
+                  </motion.span>
+                );
+              })}
+            </motion.span>
+          </div>
+        </motion.div>
+      </div> */}
     </div>
   );
 };
