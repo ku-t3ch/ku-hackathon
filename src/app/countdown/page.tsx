415c028d931860e696d6d48983362a6ff1f17de4
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2023-11-19 18:37:16 +0700

    add pocketbase

--- src/app/countdown/page.tsx
+++ src/app/countdown/page.tsx
@@ -3,6 +3,10 @@
 import BannerSlider from "@/components/BannerSlider";
 import { NextPage } from "next"
 import { useEffect, useState } from "react";
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase("https://ku-hackathon.pockethost.io");
+pb.autoCancellation(false);
 
 const banners = [
     {
@@ -29,44 +33,73 @@ const banners = [
 
 const CountDownPage: NextPage<{}> = () => {
     const [countdown, setCountdown] = useState<number[]>([0, 0, 0, 0]);
-    const [isEnd, setIsEnd] = useState<boolean>(false)
+    const [isStart, setIsStart] = useState<boolean>(false);
+    const [isEnd, setIsEnd] = useState<boolean>(false);
     const unitTime = ["days", "hours", "minutes", "seconds"];
 
+    (async() => {
+        const record = await pb.collection('hackathon').getOne('3oojmx9qvh0l4mg');
+        setIsStart(record.isStarted);
+    })();
+
     useEffect(() => {
-        setInterval(() => {
-            if (isEnd) return;
-            const now = new Date();
-            const end = new Date('2023-11-24 18:00:00');
-            const diff = end.getTime() - now.getTime();
+        (async() => {
+            const record = await pb.collection('hackathon').getOne('3oojmx9qvh0l4mg');
+            setIsStart(record.isStarted);
+        })();
+    },[]);
 
-            if (diff <= 0) {
-                setIsEnd(true);
+    useEffect(() => {
+        pb.collection('hackathon').subscribe('3oojmx9qvh0l4mg', (e) => {
+            setIsStart(e.record.isStarted);
+        });
+    });
+    
+    useEffect(() => {
+        const countdownInterval = setInterval(() => {
+            if (isStart) {
+                if (!isEnd) {
+                    const now = new Date();
+                    const end = new Date('2023-11-24 18:00:00');
+                    const diff = end.getTime() - now.getTime();
+        
+                    if (diff <= 0) {
+                        setIsEnd(true);
+                    };
+        
+                    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                    const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+                    const minutes = Math.floor(diff / (1000 * 60)) % 60;
+                    const seconds = Math.floor(diff / 1000) % 60;
+        
+                    setCountdown([days, hours, minutes, seconds]);
+                }
+            } else {
+                setCountdown([0, 0, 0, 0]);
                 return
-            };
-
-            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-            const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
-            const minutes = Math.floor(diff / (1000 * 60)) % 60;
-            const seconds = Math.floor(diff / 1000) % 60;
-
-            setCountdown([days, hours, minutes, seconds]);
+            }
         }, 1000);
-    })
+
+        return () => {
+            clearInterval(countdownInterval);
+        };
+    });
+    
 
     return (
-        <div className="w-full flex flex-1 gap-10 h-screen relative z-[999]">
+        <div className="w-full flex flex-1 h-screen relative z-[999]">
             <BannerSlider data={banners} />
             <div className="w-full h-screen flex flex-col items-center justify-center absolute z-[10]">
-                <div className="flex flex-col lg:flex-row lg: w-1/2 items-center">
+                <div className="flex flex-col lg:flex-row lg:w-2/3 w-1/2 items-center">
                     {!isEnd ? countdown.map((v, i) => {
                         return (
-                            <div className="flex flex-col m-2 p-4 w-1/4 items-center justify-center">
+                            <div key={i} className="flex flex-col w-1/4 items-center justify-center">
                                 <div className="text-8xl font-bold">{v < 10 ? "0" : null}{v}</div>
                                 <div className="text-sm capitalize">{unitTime[i]}</div>
                             </div>
                         )
                     }) :
-                        <div className="flex flex-col m-2 p-4 w-full items-center justify-center">
+                        <div className="flex flex-col w-full items-center justify-center">
                             <div className="text-8xl font-bold">กิจกรรมจบลงแล้ว</div>
                             <div className="text-sm capitalize">ขอขอบคุณ</div>
                         </div>
